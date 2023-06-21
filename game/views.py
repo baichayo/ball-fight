f424d44423091949e0a81e17746263320e71c205
bai cha <3079273327@qq.com>
2023-06-21 23:25:32 +0800

    write first page -urls.py -views.py

--- game/views.py
+++ game/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    line1 = '<h1>我还没写完，别来逛</h1>'
+    line3 = '<a href="play">或许你想van游戏？</a>'
+    line2 = '<img src="https://pic3.zhimg.com/v2-f4ed506a49ab517aa7e1e297bb3d2d22_r.jpg">'
+
+    return HttpResponse(line1 + line3 + line2)
+
+def play(request):
+    line1 = '<h1>我知道你想玩，但你先别急</h1>'
+    line2 = '<img src="https://img.zcool.cn/community/031m0wwtadygc4aoctqrbfw3637.png">'
+    return HttpResponse(line1 + line2)
